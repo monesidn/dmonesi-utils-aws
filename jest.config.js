4e622f65a036fadecd902e561077673ef0d52609
Daniele Monesi <monesidn@gmail.com>
2021-11-25 08:58:20 +0100

    Added jest-summary-reporter

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     collectCoverage: true,
+    reporters: [
+        'default',
+        ['jest-summary-reporter', { 'failuresOnly': false }]
+    ],
     roots: ['test']
 };
